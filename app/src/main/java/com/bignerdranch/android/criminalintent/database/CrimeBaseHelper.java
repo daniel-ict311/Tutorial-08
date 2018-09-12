c693e4346bd34898a84f206fbd90d2165393776f
mudbile <eulerspill@protonmail.com>
2018-09-12 18:46:53 +1000

    database created

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeBaseHelper.java
@@ -0,0 +1,33 @@
+package com.bignerdranch.android.criminalintent.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
+
+public class CrimeBaseHelper extends SQLiteOpenHelper {
+    public static final int VERSION = 1;
+    public static final String DATABASE_NAME = "crimebase.db";
+
+    public CrimeBaseHelper(Context context){
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table " + CrimeTable.NAME + "(" +
+                                "_id integer primary key autoincrement, " +
+                                 CrimeTable.cols.UUID + ", " +
+                                CrimeTable.cols.TITLE + ", " +
+                                CrimeTable.cols.DATE + ", " +
+                                CrimeTable.cols.SOLVED +
+                ")"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
