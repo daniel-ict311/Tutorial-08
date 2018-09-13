0c0941c96672f6aa6a7dde153cf5d56f20841bf6
mudbile <eulerspill@protonmail.com>
2018-09-13 12:07:19 +1000

    added getCrimeReport method in CrimeFragment

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -17,6 +17,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.text.DateFormat;
 import java.util.Date;
 import java.util.UUID;
 
@@ -128,5 +129,31 @@ public class CrimeFragment extends Fragment {
         super.onPause();
 
         CrimeLab.get(getActivity()).updateCrime(mCrime);
+    }
+
+    private String getCrimeReport(){
+        String solvedString = null;
+        if (mCrime.isSolved()){
+            solvedString = getString(R.string.crime_report_solved);
+        } else {
+            solvedString = getString(R.string.crime_report_unsolved);
+        }
+
+        String dateFormat = "EEE, MMM dd";
+        String dateString = android.text.format.DateFormat.format(
+                dateFormat, mCrime.getDate()).toString();
+
+        String suspect = mCrime.getSuspect();
+        if (suspect == null){
+            suspect = getString(R.string.crime_report_no_suspect);
+        } else {
+            suspect = getString(R.string.crime_report_suspect, suspect);
+        }
+
+        String report = getString(R.string.crime_report, mCrime.getTitle(), dateString,
+                solvedString, suspect);
+
+        return report;
+
     }
 }
